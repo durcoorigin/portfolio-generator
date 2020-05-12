d5f4368db73f9f25a66dc0ac588b2043ee3d6b94
Chris Short <61039651+durcoorigin@users.noreply.github.com>
2020-05-12 15:41:16 -0500

    completed feaure

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
+// Modules
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+// Global Variables
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
 
-    console.log('==========================');
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-    
-};
 
-printProfileData(profileDataArgs);
+
+
+// ==========================================================================
+// const profileDataArgs = process.argv.slice(2, process.argv.length);      |
+//                                                                          |
+// const printProfileData = profileDataArr => {                             |
+//     for (let i = 0; i < profileDataArr.length; i += 1) {                 |
+//         console.log(profileDataArr[i]);                                  |
+//     }                                                                    |
+//                                                                          |
+//     console.log('==========================');                           |
+//                                                                          |
+//     profileDataArr.forEach(profileItem => console.log(profileItem));     |
+//                                                                          |
+// };                                                                       |
+//                                                                          |
+// printProfileData(profileDataArgs);                                       |
+//===========================================================================
